5b9375c1291d5cadc2713c0da924395636f520a6
vinnie-kips <36153745+vinnie-kips@users.noreply.github.com>
2020-07-29 21:56:57 +0300

    Add files via upload

---
+++ AccountTest.java
@@ -0,0 +1,16 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class AccountTest {
+
+    @org.junit.jupiter.api.Test
+    void createAcc() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void deposit() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void withdraw() {
+    }
+}
---
+++ BankTest.java
@@ -0,0 +1,34 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BankTest {
+
+    @Test
+    void getInfoTest() {
+    }
+
+    @Test
+    void createAccTest() {
+    }
+
+    @Test
+    void depositTest() {
+    }
+
+    @Test
+    void testDepositTest() {
+    }
+
+    @Test
+    void withdrawTest() {
+    }
+
+    @Test
+    void interestTest() {
+    }
+
+    @Test
+    void checkBalTest() {
+    }
+}
